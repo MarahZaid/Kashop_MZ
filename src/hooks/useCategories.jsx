aac80ae34aeb8dbfa1b23ee76f0eed51f299c012
Marah Zaid <mmzaid000@gmail.com>
2026-04-08 16:39:18 +0300

    fix error

--- src/hooks/useCategories.jsx
+++ src/hooks/useCategories.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from '../api/axiosInstance';
 import { useTranslation } from 'react-i18next';
+
 export default function useCategories() {
     const { i18n } = useTranslation();
+    
     const getCategories = async () => {
         const response = await axiosInstance.get("/Categories?limit=8");
         return response.data;
     };
 
     const query = useQuery({
-        queryKey: ["categories",i18n.language],
+        queryKey: ["categories", i18n.language],
         queryFn: getCategories,
-        staleTime: 1000 * 60 * 5
+        staleTime: 1000 * 60 * 5,
+        enabled: i18n.isInitialized 
     });
 
     return query;
